f727fbf076df44fcadcd462ed995e2ba775d803b
VLadUVV <ungefuk.vlad@mail.ru>
2025-03-09 17:06:54 +0300

    commit one day

--- src/data.ts
+++ src/data.ts
@@ -14,17 +14,14 @@ const port = process.env.PORT || 3500;
 
 app.use(express.json());
 
-// Настройка CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 // Интерфейс для CartItem
 interface CartItem {
